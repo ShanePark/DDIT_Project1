6b8ff5f1707b09f9ef6edd3ba791b7b25e38f5ff
Shane_Park <kkobuk@hotmail.co.kr>
2020-12-22 10:42:27 +0900

    JDBC 접속을 DDIT 계정으로 변경하였습니다

--- src/util/JDBCUtil.java
+++ src/util/JDBCUtil.java
@@ -24,7 +24,7 @@ public class JDBCUtil {
 	}
 
 	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
-	private String user = "PSH";
+	private String user = "DDIT";
 	private String password = "java";
 
 	private Connection con = null;
